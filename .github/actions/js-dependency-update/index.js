bb2fee25ac04cbb8b42f0e51dd45d3113f495f91
k4lv15 <kalvis.kazoks@gmail.com>
2026-08-07 02:01:27 +0300

    17-2: Add additional packages.

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,5 +1,16 @@
 const core = require("@actions/core");
+const exec = require("@actions/exec");
+const github = require("@actions/github");
 async function run() {
+
+    // parse inputs
+        // base branch
+        // target branch
+        // github token
+        // working directory
+    // exec npm update
+    // check whether package*.json are modified
+    // create PR if there are changes, otherwise - finish the workflow
     core.info("I'm a basic action!");
 }
 
