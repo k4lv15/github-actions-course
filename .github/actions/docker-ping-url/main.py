c53a15eff49babb6892ab98a98cb8cb0c09f8ea4
k4lv15 <kalvis.kazoks@gmail.com>
2026-08-10 14:16:55 +0300

    17-3: Debug even more.

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -11,6 +11,9 @@ def ping_url(url, delay, max_trials):
         if response.status_code == 200:
             return True
 
+        # show "retrying..." only if there will be another trial
+        more_retries = trials < (max_trials - 1)
+        print(f"Tried to ping {url} but failed{', retrying...' if more_retries else ''}")
         time.sleep(delay)
         trials += 1
 
