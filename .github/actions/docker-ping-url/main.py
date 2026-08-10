f6eab4e47ac38576a034346cc4857ea4af5fec2d
k4lv15 <kalvis.kazoks@gmail.com>
2026-08-10 14:09:17 +0300

    17-3: Update the workflow and main python script.

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,32 @@
+import time
+import requests
+import os
+
+def ping_url(url, delay, max_trials):
+    trials = 0
+
+    while trials < max_trials:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            return True
+
+        time.sleep(delay)
+        trials += 1
+
+    return False
+
+import os
+
+def run():
+    url = os.environ["INPUT_URL"]
+    delay = int(os.environ["INPUT_DELAY"])
+    max_trials = int(os.environ["INPUT_MAX_TRIALS"])
+
+    success = ping_url(url, delay, max_trials)
+
+    if not success:
+        raise Exception(f"Failed to ping {url}")
+
 if __name__ == "__main__":
-    print("Hello world")
+    run()
